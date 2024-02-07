21978f75b0041f3ef925270c5b5a64d8562e1921
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-02-06 22:07:29 -0500

    Set code for cards

---
+++ Assets/Scripts/CardDatabase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDatabase : MonoBehaviour
+{
+   public static List<Card> cardList = new List<Card>();
+
+    void Awake()
+    {
+        cardList.Add(new Card(0, "None", 0,0,"None"));
+        cardList.Add(new Card(1, "Human", 2, 1, "This is a human"));
+        cardList.Add(new Card(2, "Elf", 3, 3, "This is a elf"));
+        cardList.Add(new Card(0, "Dwarf", 4, 4, "This is a dwarf"));
+        cardList.Add(new Card(0, "Troll", 5, 5, "This is a troll"));
+    }
+}
