b4a7f8f2bd503c3caa5fc3d4391d663441f9fced
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-02-08 01:17:27 -0500

    card DB done

---
+++ Assets/Scripts/DisplayCard.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DisplayCard : MonoBehaviour
+{
+    public List<Card> Displaycard = new List<Card>();
+    public int displayId;
+
+    public int id;
+    public string cardName;
+    public int cost;
+    public int power;
+    public string cardDescription;
+
+    public Text nameText;
+    public Text costText;
+    public Text powerText;
+    public Text descriptionText;
+
+
+
+    void Start()
+    {
+        Displaycard[0] = CardDatabase.cardList[displayId];
+    }
+
+   
+    void Update()
+    {
+        id = Displaycard[0].id;
+        cardName = Displaycard[0].cardName;
+        cost = Displaycard[0].cost;
+        power = Displaycard[0].power;
+        cardDescription = Displaycard[0].cardDescription;
+
+        nameText.text = " " + cardName;
+        costText.text = " " + cost;
+        powerText.text = " " + power;
+        descriptionText.text = " " + cardDescription;
+
+    }
+}
